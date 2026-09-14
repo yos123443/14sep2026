9186b2925181815bdaa197663e2bd06c04298661
yos123443 <yoshuaynateflores@gmail.com>
2026-09-14 09:23:27 -0500

    Add files via upload

---
+++ mi clase 4.java
@@ -0,0 +1,29 @@
+class Variables {
+public static void main(String[] args) {
+byte byteDato = 100;
+short shortDato = 1000;
+int intDato = 1000000;
+long longDato = 1000000000000L;
+float floatDato = 100.25f;
+double doubleDato = 2000.35;
+boolean booleanDato = true;
+char charDato = '\u0040';
+
+System.out.print("Dato byte: ");
+System.out.println(byteDato);
+System.out.print("Dato short: ");
+System.out.println(shortDato);
+System.out.print("Dato int: ");
+System.out.println(intDato);
+System.out.print("Dato long: ");
+System.out.println(longDato);
+System.out.print("Dato float: ");
+System.out.println(floatDato);
+System.out.print("Dato double: ");
+System.out.println(doubleDato);
+System.out.print("Dato boolean: ");
+System.out.println(booleanDato);
+System.out.print("Dato char: ");
+System.out.println(charDato);
+} // Fin del método main
+} // Fin de la clase Variables
