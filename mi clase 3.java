09b616067bb9b25724a51ac8dce3b7b8ce507fec
yos123443 <yoshuaynateflores@gmail.com>
2026-09-14 09:15:49 -0500

    Add files via upload

---
+++ mi clase 3.java
@@ -0,0 +1,11 @@
+class SecuenciasDeEscape {
+public static void main(String[] args) {
+System.out.println("Texto 1\nTexto 2");
+System.out.println("Texto 3\tTexto 4");
+System.out.println("Texto 5\rTexto 6");
+System.out.println(
+"\"Texto 7\" Texto 8");
+System.out.println(
+"Texto 9\\ Texto 10");
+} // Fin del método main
+} // Fin de la clase SecuenciasDeEscape
