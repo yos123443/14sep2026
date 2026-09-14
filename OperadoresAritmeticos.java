deac5944698ee87c7c01655d14af5d082cef8a0a
yos123443 <yoshuaynateflores@gmail.com>
2026-09-14 10:22:05 -0500

    Add files via upload

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,13 @@
+class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int numero1 = 7;
+        int numero2 = 5;
+
+        System.out.println("Operaciones con \"numero1\" y \"numero2\"");
+        System.out.println(numero1 + numero2);
+        System.out.println(numero1 - numero2);
+        System.out.println(numero1 * numero2);
+        System.out.println(numero1 / numero2);
+        System.out.println(numero1 % numero2);
+    } // Fin del método main
+} // Fin de la clase OperadoresAritmeticos
