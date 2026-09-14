f5e08aa391e6044c9a14af25cde9adc18ff2be97
yos123443 <yoshuaynateflores@gmail.com>
2026-09-14 09:50:04 -0500

    Add files via upload

---
+++ classse 5.java
@@ -0,0 +1,9 @@
+class Concatenacion {
+    public static void main(String[] args) {
+        String nombre = "John";
+        String primerApellido = "Doe";
+        String nombreConApellido = nombre + " " + primerApellido;
+
+        System.out.println(nombreConApellido);
+    } // Fin del método main
+} // Fin de la clase Concatenacion
