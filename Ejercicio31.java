b9c91eb4b38fc07e2e6bc4c62cf5ae7dba1318bb
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:53:00 -0500

    Add files via upload

---
+++ Ejercicio31.java
@@ -0,0 +1,8 @@
+class Ejercicio31 {
+    public static void main(String[] args) {
+        //31.- SUELDO SEMANAL
+        double pago_hora = 22.50;
+        double horas_trab = 44;
+        System.out.println( pago_hora * horas_trab );
+    }
+}
