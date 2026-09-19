52356841364c796a2348e39b577a603e716a8cd6
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 14:51:02 -0500

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,9 @@
+class Ejercicio2 {
+    public static void main(String[] args) {
+        //2.- PRODUCTO Y COCIENTE
+        int a = 36;
+        int b = 6;
+        System.out.println( a * b );
+        System.out.println( a / b );
+    }
+}
