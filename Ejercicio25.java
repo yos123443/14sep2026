aad52fb41f31ea8e82dc52bed15591158b8678b3
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:41:20 -0500

    Add files via upload

---
+++ Ejercicio25.java
@@ -0,0 +1,8 @@
+class Ejercicio25 {
+    public static void main(String[] args) {
+        //25.- TIEMPO DE VIAJE
+        double distancia = 360;
+        double velocidad = 80;
+        System.out.println( distancia / velocidad );
+    }
+}
