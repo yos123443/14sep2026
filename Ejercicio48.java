4d901a37f7ae08ffa4d9e1921638a159ceeadebb
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:19:23 -0500

    Add files via upload

---
+++ Ejercicio48.java
@@ -0,0 +1,8 @@
+class Ejercicio48 {
+    public static void main(String[] args) {
+        //48.- HIPOTENUSA
+        double cateto1 = 9;
+        double cateto2 = 12;
+        System.out.println( Math.sqrt(cateto1 * cateto1 + cateto2 * cateto2) );
+    }
+}
