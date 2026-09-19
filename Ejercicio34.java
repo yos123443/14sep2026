fe03c25438f468b5ac34383ffd4f53d1cf15e77b
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:59:32 -0500

    Add files via upload

---
+++ Ejercicio34.java
@@ -0,0 +1,9 @@
+class Ejercicio34 {
+    public static void main(String[] args) {
+        //34.- PRECIO CON IGV
+        double valor_venta = 850;
+        double igv_calc = valor_venta * 0.18;
+        System.out.println( igv_calc );
+        System.out.println( valor_venta + igv_calc );
+    }
+}
