7bf2e600ee503c53031b8727b0d9095f53a193c8
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:14:42 -0500

    Add files via upload

---
+++ Ejercicio44.java
@@ -0,0 +1,8 @@
+class Ejercicio44 {
+    public static void main(String[] args) {
+        //44.- LEY DE OHM
+        double voltaje = 220;
+        double resistencia = 44;
+        System.out.println( voltaje / resistencia );
+    }
+}
