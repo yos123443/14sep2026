f7a976cd6e6e6bf07660eb8fa71934140131d183
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:29:05 -0500

    Add files via upload

---
+++ Ejercicio17.java
@@ -0,0 +1,8 @@
+class Ejercicio17 {
+    public static void main(String[] args) {
+        //17.- ÁREA DE UN ROMBO
+        int D_mayor = 18;
+        int d_menor = 10;
+        System.out.println( (D_mayor * d_menor) / 2 );
+    }
+}
