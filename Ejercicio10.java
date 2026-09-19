c74dad592b7cc8c6ca6dd368e7e7c114ac58f705
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:07:49 -0500

    Add files via upload

---
+++ Ejercicio10.java
@@ -0,0 +1,9 @@
+class Ejercicio10 {
+    public static void main(String[] args) {
+        //10.- EXPRESIÓN FRACCIONARIA
+        int a = 18;
+        int b = 7;
+        int c = 5;
+        System.out.println( (double)(a + b) / c );
+    }
+}
