4f8233059c7aabfd4aed5ca3157cfd47c3fb3800
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:11:59 -0500

    Add files via upload

---
+++ Ejercicio13.java
@@ -0,0 +1,8 @@
+class Ejercicio13 {
+    public static void main(String[] args) {
+        //13.- ÁREA DE UN RECTÁNGULO
+        int base = 18;
+        int altura = 7;
+        System.out.println( base * altura );
+    }
+}
