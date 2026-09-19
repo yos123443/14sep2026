8a6a11353aaf944ee3255288b9a88e379f3fd160
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:08:53 -0500

    Add files via upload

---
+++ Ejercicio40.java
@@ -0,0 +1,9 @@
+class Ejercicio40 {
+    public static void main(String[] args) {
+        //40.- PRECIO DE VENTA
+        double costo_tot = 4625;
+        double ganancia = costo_tot * 0.25;
+        System.out.println( ganancia );
+        System.out.println( costo_tot + ganancia );
+    }
+}
