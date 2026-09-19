c3e363151df05353dc088b314d3aa8f6d289c7ae
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:45:05 -0500

    Add files via upload

---
+++ Ejercicio28.java
@@ -0,0 +1,7 @@
+class Ejercicio28 {
+    public static void main(String[] args) {
+        //28.- CELSIUS A FAHRENHEIT
+        double tempC = 28;
+        System.out.println( (tempC * 9.0 / 5.0) + 32 );
+    }
+}
