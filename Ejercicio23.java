d1b878fbd0aea577d7d2fb5271edbcd22c111798
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:38:59 -0500

    Add files via upload

---
+++ Ejercicio23.java
@@ -0,0 +1,8 @@
+class Ejercicio23 {
+    public static void main(String[] args) {
+        //23.- DISTANCIA RECORRIDA
+        double velocidad = 85;
+        double tiempo = 3.5;
+        System.out.println( velocidad * tiempo );
+    }
+}
