4426d66b54a2d74aa8c1dbacf8ac23d970601bc0
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:13:28 -0500

    Add files via upload

---
+++ Ejercicio43.java
@@ -0,0 +1,8 @@
+class Ejercicio43 {
+    public static void main(String[] args) {
+        //43.- TRABAJO MECÁNICO
+        double f_mecanica = 150;
+        double dist_mecanica = 18;
+        System.out.println( f_mecanica * dist_mecanica );
+    }
+}
