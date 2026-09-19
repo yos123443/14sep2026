f4a261052aed5a5b375d683648a0fd7f05388c41
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:49:05 -0500

    Add files via upload

---
+++ Ejercicio29.java
@@ -0,0 +1,7 @@
+class Ejercicio29 {
+    public static void main(String[] args) {
+        //29.- FAHRENHEIT A CELSIUS
+        double tempF = 95;
+        System.out.println( (tempF - 32) * 5.0 / 9.0 );
+    }
+}
