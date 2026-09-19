3be853323c2b3e3c62e4745c0b51e29901abe774
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:51:04 -0500

    Add files via upload

---
+++ Ejercicio30.java
@@ -0,0 +1,8 @@
+class Ejercicio30 {
+    public static void main(String[] args) {
+        //30.- CONSUMO DE COMBUSTIBLE
+        double km = 525;
+        double litros = 35;
+        System.out.println( km / litros );
+    }
+}
