a0c427b4c776285aeef0f5b7797d3a3a3c0c6f8e
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:03:10 -0500

    Add files via upload

---
+++ Ejercicio8.java
@@ -0,0 +1,9 @@
+class Ejercicio8 {
+    public static void main(String[] args) {
+        //8.- OPERACIONES COMBINADAS
+        int a = 12;
+        int b = 8;
+        int c = 4;
+        System.out.println( (a + b) * c / 2 );
+    }
+}
