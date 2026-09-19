ee7f328e88e5f4a692537457a70eb782c1e8576a
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:12:07 -0500

    Add files via upload

---
+++ Ejercicio42.java
@@ -0,0 +1,8 @@
+class Ejercicio42 {
+    public static void main(String[] args) {
+        //42.- PRESIÓN
+        double fuerza = 720;
+        double area = 24;
+        System.out.println( fuerza / area );
+    }
+}
