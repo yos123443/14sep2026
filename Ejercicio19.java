2d9436d95f9f7f3c96780be1ec19fc7bc8b8f5d8
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:32:40 -0500

    Add files via upload

---
+++ Ejercicio19.java
@@ -0,0 +1,7 @@
+class Ejercicio19 {
+    public static void main(String[] args) {
+        //19.- LONGITUD DE CIRCUNFERENCIA
+        double radio = 8;
+        System.out.println( 2 * Math.PI * radio );
+    }
+}
