c96569d742b30bc62d20107d83d1f9bc21efc7c7
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:24:34 -0500

    Add files via upload

---
+++ Ejercicio14.java
@@ -0,0 +1,8 @@
+class Ejercicio14 {
+    public static void main(String[] args) {
+        //14.- PERÍMETRO DE UN RECTÁNGULO
+        int largo = 22;
+        int ancho = 9;
+        System.out.println( 2 * (largo + ancho) );
+    }
+}
