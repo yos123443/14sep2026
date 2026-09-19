f1861c06da49e20b6cec48f69609810e2defe5f0
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:33:56 -0500

    Add files via upload

---
+++ Ejercicio20.java
@@ -0,0 +1,7 @@
+class Ejercicio20 {
+    public static void main(String[] args) {
+        //20.- VOLUMEN DE UN CUBO
+        int arista = 5;
+        System.out.println( arista * arista * arista );
+    }
+}
