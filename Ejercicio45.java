7f823608cfa823ed36c81e62147c87774fb996ba
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:15:52 -0500

    Add files via upload

---
+++ Ejercicio45.java
@@ -0,0 +1,8 @@
+class Ejercicio45 {
+    public static void main(String[] args) {
+        //45.- POTENCIA ELÉCTRICA
+        double voltaje = 220;
+        double corriente = 5;
+        System.out.println( voltaje * corriente );
+    }
+}
