cd7bf1ad37b05c0e7b900812cb2ed63f6b5ea7db
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:05:13 -0500

    Add files via upload

---
+++ Ejercicio38.java
@@ -0,0 +1,9 @@
+class Ejercicio38 {
+    public static void main(String[] args) {
+        //38.- REPARTO PORCENTUAL
+        double monto_total = 9000;
+        System.out.println( monto_total * 0.40 );
+        System.out.println( monto_total * 0.35 );
+        System.out.println( monto_total * 0.25 );
+    }
+}
