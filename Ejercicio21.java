3fcf2de4cc29be791adfb3356ed31f5bf01f87d2
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:35:47 -0500

    Add files via upload

---
+++ Ejercicio21.java
@@ -0,0 +1,9 @@
+class Ejercicio21 {
+    public static void main(String[] args) {
+        //21.- VOLUMEN DE UN PRISMA RECTANGULAR
+        int largo = 12;
+        int ancho = 6;
+        int altura = 4;
+        System.out.println( largo * ancho * altura );
+    }
+}
