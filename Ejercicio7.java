2214140de40bbbcc4389049c3c5693ee9d324e4e
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:02:07 -0500

    Add files via upload

---
+++ Ejercicio7.java
@@ -0,0 +1,10 @@
+class Ejercicio7 {
+    public static void main(String[] args) {
+        //7.- PROMEDIO DE CUATRO PRÁCTICAS
+        double p1 = 15;
+        double p2 = 13;
+        double p3 = 18;
+        double p4 = 16;
+        System.out.println( (p1 + p2 + p3 + p4) / 4 );
+    }
+}
