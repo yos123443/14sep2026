b8daa41ff22139d045698a05e42158864cd77bf0
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:27:42 -0500

    Add files via upload

---
+++ Ejercicio16.java
@@ -0,0 +1,9 @@
+class Ejercicio16 {
+    public static void main(String[] args) {
+        //16.- ÁREA DE UN TRAPECIO
+        int B_mayor = 20;
+        int b_menor = 12;
+        int altura = 8;
+        System.out.println( ((B_mayor + b_menor) * altura) / 2 );
+    }
+}
