fc8ca09f3837194bf21f674ff6f1585a1413f388
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:04:08 -0500

    Add files via upload

---
+++ Ejercicio37.java
@@ -0,0 +1,11 @@
+class Ejercicio37 {
+    public static void main(String[] args) {
+        //37.- INTERÉS SIMPLE
+        double capital = 5000;
+        double tasa = 0.08;
+        double tiempo_anos = 3;
+        double interes = capital * tasa * tiempo_anos;
+        System.out.println( interes );
+        System.out.println( capital + interes );
+    }
+}
