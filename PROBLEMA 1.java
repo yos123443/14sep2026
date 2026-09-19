0ee3f17901ca7381a64724d3c8c62a5e8d4aa6ba
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 14:39:44 -0500

    Add files via upload

---
+++ PROBLEMA 1.java
@@ -0,0 +1,11 @@
+class Practica {
+    public static void main(String[] args) {
+        
+        //1.- SUMA Y DIFERENCIA
+        int a = 48;
+        int b = 17;
+        System.out.println( a + b );
+        System.out.println( a - b );
+        
+    }
+}
