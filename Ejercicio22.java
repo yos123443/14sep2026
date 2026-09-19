f49f46eec3dc7e63081a7382e4e94e2806e17e9d
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:37:37 -0500

    Add files via upload

---
+++ Ejercicio22.java
@@ -0,0 +1,8 @@
+class Ejercicio22 {
+    public static void main(String[] args) {
+        //22.- VOLUMEN DE UN CILINDRO
+        double radio = 4;
+        double altura = 10;
+        System.out.println( Math.PI * radio * radio * altura );
+    }
+}
