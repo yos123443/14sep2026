a60eca737d3f5642901d0012ac5b94d1480958ad
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 14:54:55 -0500

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,9 @@
+class Ejercicio4 {
+    public static void main(String[] args) {
+        //4.- DOBLE, TRIPLE Y MITAD
+        int numero = 24;
+        System.out.println( numero * 2 );
+        System.out.println( numero * 3 );
+        System.out.println( numero / 2 );
+    }
+}
