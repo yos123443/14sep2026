7ab7b21835cb0f4e171875125fe2fdc245aa01b5
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 14:56:47 -0500

    Add files via upload

---
+++ Ejercicio5.java
@@ -0,0 +1,8 @@
+class Ejercicio5 {
+    public static void main(String[] args) {
+        //5.- CUADRADO Y CUBO
+        int numero = 7;
+        System.out.println( numero * numero );
+        System.out.println( numero * numero * numero );
+    }
+}
