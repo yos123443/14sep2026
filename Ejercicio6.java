485ae22046b2f5baf0587f66d946ed83d96a7500
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 14:58:20 -0500

    Add files via upload

---
+++ Ejercicio6.java
@@ -0,0 +1,9 @@
+class Ejercicio6 {
+    public static void main(String[] args) {
+        //6.- PROMEDIO DE TRES NOTAS
+        double nota1 = 14;
+        double nota2 = 17;
+        double nota3 = 16;
+        System.out.println( (nota1 + nota2 + nota3) / 3 );
+    }
+}
