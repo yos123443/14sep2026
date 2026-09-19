5510cdcedf7c83aed2b347783df4b2e747deb977
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:10:48 -0500

    Add files via upload

---
+++ Ejercicio41.java
@@ -0,0 +1,8 @@
+class Ejercicio41 {
+    public static void main(String[] args) {
+        //41.- DENSIDAD
+        double masa = 540;
+        double volumen = 60;
+        System.out.println( masa / volumen );
+    }
+}
