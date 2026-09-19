99cef1b42a5a691fdc2e64e9b075c8faebc4b0bc
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:40:15 -0500

    Add files via upload

---
+++ Ejercicio24.java
@@ -0,0 +1,8 @@
+class Ejercicio24 {
+    public static void main(String[] args) {
+        //24.- VELOCIDAD PROMEDIO
+        double distancia = 540;
+        double tiempo = 6;
+        System.out.println( distancia / tiempo );
+    }
+}
