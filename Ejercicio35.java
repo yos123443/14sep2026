9b7a33e9f60a410525f7173bd2018207af7de8f5
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:01:28 -0500

    Add files via upload

---
+++ Ejercicio35.java
@@ -0,0 +1,9 @@
+class Ejercicio35 {
+    public static void main(String[] args) {
+        //35.- DESCUENTO E IGV
+        double precio_base = 1600;
+        double con_descuento = precio_base * 0.90;
+        double precio_final_igv = con_descuento * 1.18;
+        System.out.println( precio_final_igv );
+    }
+}
