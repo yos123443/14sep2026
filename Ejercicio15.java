f712c05e09c186929349ac97f408c79397702e3e
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:26:14 -0500

    Add files via upload

---
+++ Ejercicio15.java
@@ -0,0 +1,8 @@
+class Ejercicio15 {
+    public static void main(String[] args) {
+        //15.- ÁREA DE UN TRIÁNGULO
+        int base = 16;
+        int altura = 11;
+        System.out.println( (base * altura) / 2 );
+    }
+}
