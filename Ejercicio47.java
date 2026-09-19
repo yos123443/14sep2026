c638d9719bc92df0cbb581d0a6ed39a0632e2200
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:18:14 -0500

    Add files via upload

---
+++ Ejercicio47.java
@@ -0,0 +1,8 @@
+class Ejercicio47 {
+    public static void main(String[] args) {
+        //47.- ÍNDICE DE MASA CORPORAL
+        double peso = 72;
+        double estatura = 1.74;
+        System.out.println( peso / (estatura * estatura) );
+    }
+}
