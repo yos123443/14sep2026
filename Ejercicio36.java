8a5b8f6531a533e0b0bdf91494303079ef86b6d3
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:03:03 -0500

    Add files via upload

---
+++ Ejercicio36.java
@@ -0,0 +1,10 @@
+class Ejercicio36 {
+    public static void main(String[] args) {
+        //36.- COMISIÓN DE VENTA
+        double venta_total = 12500;
+        double comision = venta_total * 0.06;
+        double sueldo_base = 1400;
+        System.out.println( comision );
+        System.out.println( sueldo_base + comision );
+    }
+}
