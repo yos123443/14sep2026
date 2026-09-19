482b15806d106857d286dcecd1d498b066c87cf1
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:42:36 -0500

    Add files via upload

---
+++ Ejercicio26.java
@@ -0,0 +1,10 @@
+class Ejercicio26 {
+    public static void main(String[] args) {
+        //26.- CONVERSIÓN DE HORAS
+        double horas = 7.5;
+        double minutos = horas * 60;
+        double segundos = minutos * 60;
+        System.out.println( minutos );
+        System.out.println( segundos );
+    }
+}
