8b7b407963861f72c1dbf357c17329a133323b17
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:56:32 -0500

    Add files via upload

---
+++ Ejercicio32.java
@@ -0,0 +1,8 @@
+class Ejercicio32 {
+    public static void main(String[] args) {
+        //32.- IMPORTE DE UNA COMPRA
+        int cuadernos = 18;
+        double precio_cuad = 7.80;
+        System.out.println( cuadernos * precio_cuad );
+    }
+}
