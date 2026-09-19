807fec03b5c868843f2b258382e995fa89daaa40
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:58:21 -0500

    Add files via upload

---
+++ Ejercicio33.java
@@ -0,0 +1,9 @@
+class Ejercicio33 {
+    public static void main(String[] args) {
+        //33.- DESCUENTO COMERCIAL
+        double precio_art = 1200;
+        double desc_com = precio_art * 0.15;
+        System.out.println( desc_com );
+        System.out.println( precio_art - desc_com );
+    }
+}
