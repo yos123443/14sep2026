20e0c3e5beef45effc0b4b24ba4f6fed9a950952
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 16:07:19 -0500

    Add files via upload

---
+++ Ejercicio39.java
@@ -0,0 +1,8 @@
+class Ejercicio39 {
+    public static void main(String[] args) {
+        //39.- COSTO DE PRODUCCIÓN
+        int unidades = 250;
+        double costo_uni = 18.50;
+        System.out.println( unidades * costo_uni );
+    }
+}
