d80cc57a423bb9ecf00d58c385931845e5ca3230
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 14:53:13 -0500

    Add files via upload

---
+++ Ejercicio3.java
@@ -0,0 +1,9 @@
+class Ejercicio3 {
+    public static void main(String[] args) {
+        //3.- RESIDUO DE UNA DIVISIÓN
+        int dividendo = 157;
+        int divisor = 12;
+        System.out.println( dividendo / divisor );
+        System.out.println( dividendo % divisor );
+    }
+}
