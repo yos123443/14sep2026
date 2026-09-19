5f902f33ff187b5ce46690127f5c8d1c476e8839
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:06:35 -0500

    Add files via upload

---
+++ Ejercicio9.java
@@ -0,0 +1,9 @@
+class Ejercicio9 {
+    public static void main(String[] args) {
+        //9.- EXPRESIÓN CON PARÉNTESIS
+        int a = 20;
+        int b = 6;
+        int c = 3;
+        System.out.println( (a - b) * (a + c) );
+    }
+}
