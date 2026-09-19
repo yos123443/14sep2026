122a138fc6bfb6bc6ff058c1a644772081642ca6
yos123443 <yoshuaynateflores@gmail.com>
2026-09-19 15:30:45 -0500

    Add files via upload

---
+++ Ejercicio18.java
@@ -0,0 +1,7 @@
+class Ejercicio18 {
+    public static void main(String[] args) {
+        //18.- ÁREA DE UN CÍRCULO
+        double radio = 6;
+        System.out.println( Math.PI * radio * radio );
+    }
+}
